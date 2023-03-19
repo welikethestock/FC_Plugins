f4c2682bb7c80979a0d19deb0fa2c95a4e04e901
welikethestock <>
2023-03-19 21:49:36 +0100

    Edit log a little

--- PluginLoader/Util/Log.cpp
+++ PluginLoader/Util/Log.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <time.h>
 
 extern "C" __declspec(dllexport)
 void SDK_Util_Log(const char *Message)
 {
-    printf(Message);
+    char        Buffer[26];
+    time_t      Time = time(NULL);
+    struct tm   *Info = localtime(&Time);
+
+    strftime(Buffer, sizeof(Buffer), "%m/%d/%Y %H:%M", Info);
+    printf("[%s]: %s", Buffer, Message);
 }
