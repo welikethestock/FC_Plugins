28219bcb62e189bc07cd2fd25f80d408c5c1e436
welikethestock <>
2023-03-19 17:36:09 +0100

    remove annoying warnings

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -1,23 +1,23 @@
 BIN=/home/razor/msvc/bin/x64 . /home/razor/msvc-wine/msvcenv-native.sh
 
-clang   --target=x86_64-windows-msvc -Wl,/subsystem:windows,/DLL -O2 -Wno-pragma-pack -Wno-macro-redefined \
+clang   --target=x86_64-windows-msvc -Wl,/subsystem:windows,/DLL -O2 -Wno-deprecated-declarations -Wno-pragma-pack -Wno-macro-redefined \
         -I./../SDK \
         -fuse-ld=lld -o Loader.dll \
         -D_USRDLL -D_WINDLL \
         ../Loader/DllMain.cpp ../Loader/Plugins.cpp
 
-clang   --target=x86_64-windows-msvc -Wl,/subsystem:windows,/DLL -O2 -Wno-pragma-pack -Wno-macro-redefined \
+clang   --target=x86_64-windows-msvc -Wl,/subsystem:windows,/DLL -O2 -Wno-deprecated-declarations -Wno-pragma-pack -Wno-macro-redefined \
         -I./../SDK \
         -fuse-ld=lld -o HighPingKickFix.dll \
         -D_USRDLL -D_WINDLL \
         ../Plugins/Universal/HighPingKickFix/DllMain.cpp
 
-clang   --target=x86_64-windows-msvc -Wl,/subsystem:windows,/DLL -O2 -Wno-pragma-pack -Wno-macro-redefined \
+clang   --target=x86_64-windows-msvc -Wl,/subsystem:windows,/DLL -O2 -Wno-deprecated-declarations -Wno-pragma-pack -Wno-macro-redefined \
         -fuse-ld=lld -o dbdata.dll \
         -D_USRDLL -D_WINDLL \
         ../Proxy/DBData.cpp
 
-clang   --target=x86_64-windows-msvc -Wl,/subsystem:console -Wno-pragma-pack -Wno-macro-redefined \
+clang   --target=x86_64-windows-msvc -Wl,/subsystem:console -Wno-deprecated-declarations -Wno-pragma-pack -Wno-macro-redefined \
         -fuse-ld=lld -o TestApp.exe \
         ../TestApp/Main.c
 
