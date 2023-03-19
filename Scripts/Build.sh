2a97915e30375e07fb06c200ec69f9d5ed53ed20
welikethestock <>
2023-03-19 21:52:11 +0100

    update build script

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -1,9 +1,13 @@
 BIN=/home/razor/msvc/bin/x64 . /home/razor/msvc-wine/msvcenv-native.sh
 
+echo "Compiling dbdata.dll..."
+
 clang   --target=x86_64-windows-msvc -Wl,/subsystem:windows,/DLL -O3 -Wno-deprecated-declarations -Wno-pragma-pack -Wno-macro-redefined \
         -fuse-ld=lld -o dbdata.dll \
         -D_USRDLL -D_WINDLL \
         ../Proxy/DBData.cpp
+
+echo "Compiling PluginLoader.dll..."
 
 clang   --target=x86_64-windows-msvc -Wl,/subsystem:windows,/DLL -O3 -Wno-deprecated-declarations -Wno-pragma-pack -Wno-macro-redefined -Wno-format-security \
         -I./../SDK \
@@ -12,11 +16,15 @@ clang   --target=x86_64-windows-msvc -Wl,/subsystem:windows,/DLL -O3 -Wno-deprec
         ../PluginLoader/DllMain.cpp ../PluginLoader/Plugins.cpp \
         ../PluginLoader/Util/Signatures.cpp ../PluginLoader/Util/Stubs.cpp ../PluginLoader/Util/Log.cpp
 
+echo "Compiling HighPingKickFix.dll..."
+
 clang   --target=x86_64-windows-msvc -Wl,/subsystem:windows,/DLL -O3 -Wno-deprecated-declarations -Wno-pragma-pack -Wno-macro-redefined \
         -I./../SDK \
         -fuse-ld=lld -o HighPingKickFix.dll \
         -D_USRDLL -D_WINDLL \
         ../Plugins/Universal/HighPingKickFix/Entry.cpp
+
+echo "Done compiling..."
 
 rm "/home/razor/.local/share/bottles/bottles/Ubisoft/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/games/Far Cry 5/bin/PluginLoader.dll"
 rm "/home/razor/.local/share/bottles/bottles/Ubisoft/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/games/Far Cry 5/plugins/HighPingKickFix.dll"
